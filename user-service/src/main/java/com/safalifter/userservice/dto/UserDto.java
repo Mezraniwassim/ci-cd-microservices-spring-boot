f6520f5dcf4fdefce9302e56846b50047b324dde
wassim <mezrani.wassim@isitc.u-sousse.tn>
2024-10-27 18:03:26 +0100

    Add Dockerfile for auth-service, gateway, job-service, file-storage, user-service, config-server, eureka-server, and notification-service
    Signed-off-by: wassim <mezrani.wassim@isitc.u-sousse.tn>

---
+++ user-service/src/main/java/com/safalifter/userservice/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.safalifter.userservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.safalifter.userservice.model.UserDetails;
+import lombok.Data;
+
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+    private String id;
+    private String username;
+    private String email;
+    private UserDetails userDetails;
+}
